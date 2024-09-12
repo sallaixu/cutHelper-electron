2fe4863d54e553fc667ab7b6671af79fa1f1e59a
sallai <sallai@aliyun.com>
2024-09-12 08:41:28 +0800

    更新快捷键

---
+++ src/renderer/src/components/setting/EditHotKey.vue
@@ -0,0 +1,27 @@
+<template>
+ <div style="z-index:99999;height: 300px; width: 100%;">
+    <div v-for="item in pressKeys">
+        <a-tag color="green">{{ item }}</a-tag>
+    </div>
+ </div>
+</template>
+
+<script setup>
+import { ref,onMounted } from 'vue';
+const pressKeys = ref([])
+
+window.addEventListener('keydown', function(event) {
+            var key = event.key; // 获取按键
+            if (!pressKeys.value.includes(key)) {
+                pressKeys.value.push(key)
+            }
+});
+
+
+
+
+</script>
+
+<style scoped>
+
+</style>
--- src/renderer/src/components/setting/HotKey.vue
+++ src/renderer/src/components/setting/HotKey.vue
@@ -2,32 +2,25 @@
  <div>
     <label>唤起应用/最小化:</label><a-input v-model:value="hotKey.wakeup" placeholder=" ctrl + space" />
     <div>{{ pressKeys }}</div>
-    <button @click="recordSequence">修改</button>
+    <button>修改</button>
+    <edit-hot-key></edit-hot-key>
  </div>
 </template>
 
 <script setup>
 import { ref,onMounted } from 'vue';
-import Mousetrap  from 'mousetrap';
 var pressKeys = ref("")
 const hotKey = ref({
    "wakeup":""
 })
 onMounted(()=>{
     queryAppConfig()
-}) 
+})
 var queryAppConfig = () => window.electron.ipcRenderer.invoke('queryConfig').then(config => {
     console.log(config.hotkey)
     hotKey.value = config.hotkey
 })
 
-function recordSequence() {
-
-    window.addEventListener('keydown', function(event) {
-            var key = event.key; // 获取按键
-            pressKeys.value +=key
-        });
-}
 
 
 
